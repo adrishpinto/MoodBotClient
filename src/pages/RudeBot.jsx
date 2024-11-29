e4b4e1e485ed7cd888de17d424eb401369d701dd
Joshua <adrishpinto423@gmail.com>
2024-11-29 18:11:32 +0530

    client

--- src/pages/RudeBot.jsx
+++ src/pages/RudeBot.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { IoIosArrowDropupCircle } from "react-icons/io";
 import { RiRobot2Fill } from "react-icons/ri";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const RudeBot = () => {
   const [message, setMessage] = useState("");
   const [history, setHistory] = useState([]);
